a6a56df7210f4ea50fa660606030f1e3d1dd8ee6
AvineshJ <69710301+AvineshJ@users.noreply.github.com>
2021-12-18 13:05:28 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,29 @@
+
+<style>
+	.collapse a{
+		text-indent:10px;
+	}
+	nav#sidebar{
+		background: url(assets/uploads/<?php echo $_SESSION['system']['cover_img'] ?>) !important
+	}
+</style>
+
+<nav id="sidebar" class='mx-lt-5 bg-dark' >
+		
+		<div class="sidebar-list">
+				
+				
+				<a href="index.php?page=topics" class="nav-item nav-topics"><span class='icon-field'><i class="fa fa-comment"></i></span> Discussion</a>
+				<?php if($_SESSION['login_type'] == 1): ?>
+				
+			<?php endif; ?>
+		</div>
+
+</nav>
+<script>
+	$('.nav_collapse').click(function(){
+		console.log($(this).attr('href'))
+		$($(this).attr('href')).collapse()
+	})
+	$('.nav-<?php echo isset($_GET['page']) ? $_GET['page'] : '' ?>').addClass('active')
+</script>
